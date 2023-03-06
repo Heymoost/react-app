a458322f4719caa5de373584cc1641a78701c135
Anton Suslov <heymoost@gmail.com>
2023-03-06 16:16:24 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Suspense } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import HeaderConteiner from './components/Header/HeaderContainer';
 import { connect } from 'react-redux';
 import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
 import store from './redux/redux-store';
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
